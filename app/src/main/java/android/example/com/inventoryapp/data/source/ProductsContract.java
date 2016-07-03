0a790587e8db7888619465f986fb5560e2344130
Isabel Palomar <isabel.palomarb@gmail.com>
2016-07-03 11:09:28 -0400

    ProductsDbHelper :dancers:

---
+++ app/src/main/java/android/example/com/inventoryapp/data/source/ProductsContract.java
@@ -0,0 +1,18 @@
+package android.example.com.inventoryapp.data.source;
+
+import android.provider.BaseColumns;
+
+public class ProductsContract {
+
+    public ProductsContract(){}
+
+    public static abstract class ProductEntry implements BaseColumns {
+        public static final String TABLE_NAME = "products";
+        public static final String COLUMN_NAME_ENTRY_ID = "entryId";
+        public static final String COLUMN_NAME_PRODUCT = "name";
+        public static final String COLUMN_NAME_PRICE = "price";
+        public static final String COLUMN_NAME_QUANTITY = "quantity";
+        public static String COLUMN_NAME_NULLABLE = null;
+
+    }
+}
